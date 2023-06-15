bbf3879455f58ed8befad5397f8b521774a48d09
saad-mansuri <saadmansuri249@gmail.com>
2023-06-16 00:55:13 +0530

    sidebar and header working fine

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
     console.log('jquery loaded');
-    initMenu();
- });
+
+    let sidebar = $('.custom-sidebar').width()
+    let mainContent = $('.main-content')
+    mainContent.css('padding-left', sidebar)
+
+    let searchInputBox = $('.search-box')       
+    let searchIcon = $('.search-box__icon')
+    let closeIcon = $('.close-icon')
+    
+    $(searchIcon).on('click', ()=>{
+        searchInputBox.addClass('open')
+    })
+    $(closeIcon).on('click', ()=>{
+        searchInputBox.removeClass('open')
+    })
+});
+
  
